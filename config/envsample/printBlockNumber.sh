594e128457d158d3910822c5203a5a98189f8387
f4prime <zzz136454872@163.com>
2021-09-07 15:51:06 +0800

    add sleep in printBlockNumber

--- config/envsample/printBlockNumber.sh
+++ config/envsample/printBlockNumber.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CITA_CLOUD_RPC_ADDR=`minikube ip`:30004 CITA_CLOUD_EXECUTOR_ADDR=`minikube ip`:30005
-for a in {1..20}
+for a in {1..100}
 do
 	cldi block-number
+	sleep 0.2
 done
